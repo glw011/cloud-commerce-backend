58b90d6c9a14f85a53311df22051a0a5c96d9ce8
glw011 <glw011@email.latech.edu>
2026-07-01 12:12:55 -0500

    update: order repository single query order lookup 'findDetailById'

--- src/main/java/com/garrettw011/orderflow/order/OrderRepository.java
+++ src/main/java/com/garrettw011/orderflow/order/OrderRepository.java
@@ -1,9 +1,15 @@
 package com.garrettw011.orderflow.order;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     Page<Order> findByCustomerId(Long customerId, Pageable pageable);
+
+    // spring ignores word between 'find' & 'By' (just findById with fetch graph)
+    @EntityGraph(attributePaths = {"customer", "items", "items.product"})
+    Optional<Order> findDetailById(Long id);
 }
